658242e10585820faa343f8a84300e87ca9ec910
DirkFriedenberger <DirkFriedenberger@deutschebahn.com>
2019-05-20 20:49:35 +0200

    add skeleton for backend

---
+++ backend/src/main/java/org/dreilaenderhack/model/Person.java
@@ -0,0 +1,29 @@
+package org.dreilaenderhack.model;
+
+public class Person {
+
+	
+	private String name;
+	private Double rank;
+	private String image;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getRank() {
+		return rank;
+	}
+	public void setRank(Double rank) {
+		this.rank = rank;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+}
