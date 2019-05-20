7ebdb9bc5beacd8070a31b8d2522c1e9cd0fe4fc
Bauer Ana (BCC.IKT) <z171545@bcc.oebb.at>
2019-05-20 20:25:00 +0200

    styling ergebnisliste

--- frontend/search.js
+++ frontend/search.js
@@ -19,9 +19,12 @@
       })
       .then(function (list) {
         result.innerHTML = list.map(function (entry) {
-          return '<li>' +
-            '<img src="' + entry.picture + '">' +
-            '<span class="name">' + entry.name + '</span>' +
+          return '<li class="result">' +
+            '<img src="' + entry.picture + '" class="result__img">' +
+            '<div class="result__inner">' +
+            '<span class="result__name">' + entry.name + '</span>' +
+            '<a href="#" class="result__send-query">Anfrage senden <i class="fas fa-paper-plane"></i></a>' +
+            '</div>' +
             '</li>'
         }).join('\n')
       })
