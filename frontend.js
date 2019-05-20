365cfe707cc28041eb604ecd739d9f350036e52f
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-05-20 17:24:07 +0200

    Mocked search result

--- frontend.js
+++ frontend.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const logger = console
+const SearhController = require('./SearchController')({logger})
 
 const app = express()
 
 app.use('/', express.static('frontend'))
+app.get('/search', (req, res) => res.json(SearhController.search(req.query.value)))
 
 app.listen(8000, () => {
   logger.info('Running on port 8000')
--- frontend/search.js
+++ frontend/search.js
@@ -7,7 +7,25 @@
   form.addEventListener('submit', function (event) {
     event.preventDefault()
 
-    var search = form.elements['search'].value;
-    result.innerHTML = '<li>Suchergebnis fuer "' + search + '"</li>'
+    var search = form.elements['search'].value
+    fetch('/search?value=' + encodeURIComponent(search))
+      .then(function (result) {
+        if (!result.ok) {
+          return Promise.reject(result.statusText)
+        } else {
+          return result.json()
+        }
+      })
+      .then(function (list) {
+        result.innerHTML = list.map(function (entry) {
+          return '<li>' +
+            '<img src="' + entry.picture + '">' +
+            '<span class="name">' + entry.name + '</span>' +
+            '</li>'
+        }).join('\n')
+      })
+      .catch(function (error) {
+        alert(error)
+      })
   })
 })()
